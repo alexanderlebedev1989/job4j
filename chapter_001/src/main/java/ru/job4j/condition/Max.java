16157d4c8bed0def1e1684dc14d5051a7c7745ce
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-02-07 22:57:53 +0500

    Максимум из двух чисел [#227332]

---
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        int result = left;
+        return result;
+    }
+
+    public static int max1(int left, int right) {
+        int result1 = right;
+        return result1;
+    }
+    public static int max2(int left, int right) {
+        int result2 = left;
+        return result2;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.condition;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class MaxTest {
+
+    @Test
+    public void whenMax2To1Then() {
+        int result = Max.max(2, 1);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax1To2Then() {
+        int result = Max.max1(1, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    public void whenMax1To1Then() {
+        int result = Max.max2(1, 1);
+        assertThat(result, is(1));
+    }
+}
