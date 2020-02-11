0e0ab0f45619a139538eb681304a2eb0316f674a
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-02-11 20:13:52 +0500

    5.7. Ипотека.[#227406]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+
+    public int year(double amount, int salary, double percent) {
+        int year = 0;
+        while (amount >= 0) {
+            percent = amount * percent /100 + amount;
+            amount = percent - salary;
+            year++;
+        }
+        return year;
+    }
+}
