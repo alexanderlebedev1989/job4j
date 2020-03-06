8ee0185867c49f302f6260277e84081cb07e6ba9
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-03-06 14:10:04 +0500

    2. Scanner и чтение числа из консоли.[#227472]

--- chapter_001/src/main/java/ru/job4j/job4j/io/Eleven.java
+++ chapter_001/src/main/java/ru/job4j/job4j/io/Eleven.java
@@ -1,4 +1,6 @@
 package ru.job4j.job4j.io;
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Eleven {
@@ -7,23 +9,26 @@ public class Eleven {
         Scanner input = new Scanner(System.in);
         int s = 11;
         while (true) {
-            System.out.println("Ходит первый игрок");
-            int first = Integer.valueOf(input.nextLine());
-            s = s - first;
-            if (s <= 0) {
+            System.out.println("Игра 11");
+            System.out.println("Чтобы первый игрок сделал ход, нажмите 1.");
+            System.out.println("Чтобы второй игрок сделал ход, нажмите 2.");
+            int player = Integer.valueOf(input.nextLine());
+            if (player == 1) {
+                System.out.println("Ходит первый игрок. Введите число от 1 до 3.");
+            } else if (player == 2) {
+                System.out.println("Ходит второй игрок.Введите число от 1 до 3.");
+            }
+            int number = Integer.valueOf(input.nextLine());
+            s = s - number;
+            if (s <= 0 && player == 1) {
                 System.out.println("Выиграл первый игрок");
                 break;
-            }
-            System.out.println("Осталость " + s);
-
-            System.out.println("Ходит второй игрок");
-            int second = Integer.valueOf(input.nextLine());
-            s = s - second;
-            if (s <= 0) {
+            } else if (s <= 0 && player == 2) {
                 System.out.println("Выиграл второй игрок");
                 break;
             }
-            System.out.println("Осталось " + s);
+            System.out.println("Осталость " + s);
+            System.out.println();
         }
     }
 }
