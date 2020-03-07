de2fc2e221d5b19551ef316fc3db32c3e0e8f3fb
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-03-07 13:37:36 +0500

    3. Интерфейс Input[#227473]

---
+++ chapter_001/src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,21 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void go() {
+        System.out.println("Автобус едет в Санкт - Петербург.");
+    }
+
+    @Override
+    public void take(int passenger) {
+        System.out.println("В Санкт - Петербург едет " + passenger);
+    }
+
+    @Override
+    public int refuel(int fuel) {
+        int priceLiter = 30;
+        int totalPrice = fuel * priceLiter;
+        return totalPrice;
+    }
+}
