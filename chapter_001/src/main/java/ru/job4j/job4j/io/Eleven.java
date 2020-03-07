40a58c39d6d2be6e7d83175d8fd90466f681df05
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-03-07 12:42:13 +0500

    2. Scanner и чтение числа из консоли.[#227472]

--- chapter_001/src/main/java/ru/job4j/job4j/io/Eleven.java
+++ chapter_001/src/main/java/ru/job4j/job4j/io/Eleven.java
@@ -1,6 +1,4 @@
 package ru.job4j.job4j.io;
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
 
 public class Eleven {
@@ -11,25 +9,16 @@ public class Eleven {
         boolean isFinished = false;
         while (!isFinished) {
             System.out.println("Игра 11");
-            System.out.println("Чтобы первый игрок сделал ход, нажмите 1.");
-            System.out.println("Чтобы второй игрок сделал ход, нажмите 2.");
-            int player = Integer.valueOf(input.nextLine());
-            if (player == 1) {
-                System.out.println("Ходит первый игрок. Введите число от 1 до 3.");
-            } else if (player == 2) {
-                System.out.println("Ходит второй игрок.Введите число от 1 до 3.");
-            }
+            System.out.println("Введите число от одного до трёх");
             int number = Integer.valueOf(input.nextLine());
             s = s - number;
-            if (s <= 0 && player == 1) {
-                System.out.println("Выиграл первый игрок");
-                break;
-            } else if (s <= 0 && player == 2) {
-                System.out.println("Выиграл второй игрок");
-                break;
-            }
+            if (s == 0) {
+                System.out.println("Игра закончена");
+                isFinished = true;
+            } else {
             System.out.println("Осталость " + s);
             System.out.println();
+            }
         }
     }
 }
