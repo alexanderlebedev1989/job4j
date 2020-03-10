1b6eb59310715fdde7da5531103188a50a01fc27
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-03-10 16:48:37 +0500

    0.2. Кидаем исключение - throw new RuntimeException[#227490]

---
+++ chapter_001/src/main/java/ru/job4j/ex/Fact.java
@@ -0,0 +1,19 @@
+package ru.job4j.ex;
+
+public class Fact {
+    public static void main(String[] args) {
+        Fact fact = new Fact();
+        System.out.println(fact.calc(4));
+    }
+
+    public int calc(int n) {
+        int rsl = 1;
+        if (n < 1) {
+          throw new IllegalCallerException("value (n) must be less than 1");
+        }
+        for (int index = 1; index != n; index++) {
+            rsl *= index;
+        }
+        return rsl;
+    }
+}
