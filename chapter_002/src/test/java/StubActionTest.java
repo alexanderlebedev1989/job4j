ca374cd874e0eeb9ad2f2d63b5bbfcf6dd86371e
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-03-10 12:11:28 +0500

    8. Реализация меню за счет шаблона стратегия.[#227479]

---
+++ chapter_002/src/test/java/StubActionTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import ru.job4j.tracker.*;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class StubActionTest {
+
+    @Test
+    public void whenExit() {
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        assertThat(action.isCall(), is(true));
+    }
+}
