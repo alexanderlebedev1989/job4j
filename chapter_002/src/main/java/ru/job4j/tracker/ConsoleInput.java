135ad49bb36a67c4f10c808b903da84028b2df35
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-03-12 19:28:35 +0500

    2. Рефакторинг - Шаблон Декоратор для валидатора.[#227487]

--- chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -27,3 +27,4 @@ public class ConsoleInput implements Input {
     }
 }
 
+
--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -13,8 +13,9 @@ public class ValidateInputTest {
         ByteArrayOutputStream mem = new ByteArrayOutputStream();
         PrintStream out = System.out;
         System.setOut(new PrintStream(mem));
-        String[] data = {"one", "1"};
-        ValidateInput input = new ValidateStubInput(data);
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"one", "1"})
+        );
         input.askInt("Enter");
         assertThat(
                 mem.toString(),
@@ -28,8 +29,9 @@ public class ValidateInputTest {
         ByteArrayOutputStream mem = new ByteArrayOutputStream();
         PrintStream out = System.out;
         System.setOut(new PrintStream(mem));
-        String[] data = {"7", "5"};
-        ValidateInput input = new ValidateStubInput(data);
+        ValidateInput input = new ValidateInput(
+                new StubInput(new String[] {"7", "5"})
+        );
         input.askInt("Enter", 6);
         assertThat(
                 mem.toString(),
