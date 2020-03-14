d31b0f1524b6dcfa5ad33f518bb84e4ab6fa087f
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-03-14 14:55:58 +0500

    Сдача в кофе машине. [#34741]

---
+++ chapter_001/src/main/java/ru/job4j/sort/Machine.java
@@ -0,0 +1,20 @@
+package ru.job4j.sort;
+
+import java.util.Arrays;
+
+public class Machine {
+    private final int[] COINS = {10, 5, 2, 1};
+
+    public int[] change(int money, int price) {
+        int[] rsl = new int[100];
+        int size = 0;
+        int balance = money - price;
+        for (int coin : COINS) {
+            while ((balance - coin) >= 0) {
+                balance -= coin;
+                rsl[size++] = coin;
+            }
+        }
+        return Arrays.copyOf(rsl, size);
+    }
+}
