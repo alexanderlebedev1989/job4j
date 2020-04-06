5777f072e5d88c811eb00560502af8fe4395afb7
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-04-06 20:12:42 +0500

    0. Stream API улучшения[#227310]

--- chapter_001/src/main/java/ru/job4j/stream/School.java
+++ chapter_001/src/main/java/ru/job4j/stream/School.java
@@ -1,13 +1,20 @@
 package ru.job4j.stream;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class School {
     Map<String, Student> collect(List<Student> students) {
-        return students.stream().collect(Collectors.toMap(Student::getSurname, student -> student,
+        return students.stream().collect(Collectors.toMap(Student::getName, student -> student,
                 ((student, student2) -> student)
                 ));
+    }
+    List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream().flatMap(Stream::ofNullable).
+                takeWhile(student -> student.getScore() > bound).
+                sorted(Comparator.comparing(Student::getName)).collect(Collectors.toList());
     }
 }
--- chapter_001/src/test/java/ru/job4j/stream/SchoolTest.java
+++ chapter_001/src/test/java/ru/job4j/stream/SchoolTest.java
@@ -18,10 +18,26 @@ public class SchoolTest {
       List<Student> students = Arrays.asList(st1, st2, st3, st4);
       Map<String, Student> result = school.collect(students);
       Map<String, Student> expected = new HashMap<>();
-      expected.put(st1.getSurname(), st1);
-      expected.put(st2.getSurname(), st2);
-      expected.put(st3.getSurname(), st3);
+      expected.put(st1.getName(), st1);
+      expected.put(st2.getName(), st2);
+      expected.put(st3.getName(), st3);
       assertThat(result, is(expected));
    }
+
+    @Test
+    public void whenListSortAndWhileAndNull() {
+        School school = new School();
+        int bound = 50;
+        Student st1 = new Student(90, "Petrov");
+        Student st2 = null;
+        Student st3 = new Student(100, "Ivanov");
+        Student st4 = null;
+        Student st5 = new Student(50, "Sidorov");
+        Student st6 = new Student(90, "Sokolov");
+        List<Student> students = Arrays.asList(st1, st2, st3, st4, st5, st6);
+        List<Student> result = school.levelOf(students, bound);
+        List<Student> expected = Arrays.asList(st3, st1);
+        assertThat(result, is(expected));
+    }
 }
 
