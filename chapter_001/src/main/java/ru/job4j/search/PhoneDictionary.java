586f91e9469cc6c484544e39d3f1faaa8843489b
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-04-07 14:00:51 +0500

    2. Local-Variable Type Inference[#227312]

--- chapter_001/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_001/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -15,6 +15,7 @@ public class PhoneDictionary {
      * @param key Ключ поиска.
      * @return Список подощедщих пользователей.
      */
+
     public ArrayList<Person> find(String key) {
         var result = new ArrayList<Person>();
         Predicate<Person> predicate1 = person -> person.getName().contains(key) || person.getSurname().contains(key);
