c3fd1e204f4b3da0462dab1ddf54aaf117f5c9b3
alexanderlebedev1989 <AlexanderLebedev1989@gmail.com>
2020-06-24 14:05:39 +0500

    4.5. Travis CI и PostgreSQL [#271845]

--- chapter_002/src/test/java/ru/job4j/tracker/SqlTrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/SqlTrackerTest.java
@@ -1,73 +1,73 @@
-//package ru.job4j.tracker;
-//
-//import org.junit.Test;
-//import java.util.List;
-//import static org.hamcrest.core.Is.is;
-//import static org.hamcrest.core.IsNull.nullValue;
-//import static org.junit.Assert.assertThat;
-//
-//public class SqlTrackerTest {
-//
-//    @Test
-//    public void createItem() throws Exception {
-//        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
-//            tracker.add(new Item("name"));
-//            assertThat(tracker.findByName("name").size(), is(1));
-//        }
-//    }
-//
-//    @Test
-//    public void editItem() throws Exception {
-//        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
-//            Item item = new Item("test1");
-//            tracker.add(item);
-//            tracker.replace(item.getId(), new Item("new"));
-//            assertThat(tracker.findByName("new").size(), is(1));
-//
-//        }
-//    }
-//
-//    @Test
-//    public void deleteItem() throws Exception {
-//        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
-//            Item item = new Item("test1");
-//            tracker.add(item);
-//            tracker.delete(item.getId());
-//            assertThat(tracker.findById(item.getId()), is(nullValue()));
-//
-//        }
-//    }
-//
-//    @Test
-//    public void findByName() throws Exception {
-//        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
-//            Item item = new Item("test1");
-//            tracker.add(item);
-//            List<Item> result = tracker.findByName(item.getName());
-//            assertThat(result.size(), is(1));
-//
-//        }
-//    }
-//
-//    @Test
-//    public void findById() throws Exception {
-//        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
-//            Item item = new Item("test1");
-//            tracker.add(item);
-//            Item result = tracker.findById(item.getId());
-//            assertThat(result.getName(), is("test1"));
-//
-//        }
-//    }
-//
-//    @Test
-//    public void showListItems() throws Exception {
-//        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
-//            tracker.add(new Item("test1"));
-//            tracker.add(new Item("test2"));
-//            List<Item> result = tracker.findAll();
-//            assertThat(result.size(), is(2));
-//
-//        }
-//    }
-//}
+package ru.job4j.tracker;
+
+import org.junit.Test;
+import java.util.List;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class SqlTrackerTest {
+
+    @Test
+    public void createItem() throws Exception {
+        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
+            tracker.add(new Item("name"));
+            assertThat(tracker.findByName("name").size(), is(1));
+        }
+    }
+
+    @Test
+    public void editItem() throws Exception {
+        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
+            Item item = new Item("test1");
+            tracker.add(item);
+            tracker.replace(item.getId(), new Item("new"));
+            assertThat(tracker.findByName("new").size(), is(1));
+
+        }
+    }
+
+    @Test
+    public void deleteItem() throws Exception {
+        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
+            Item item = new Item("test1");
+            tracker.add(item);
+            tracker.delete(item.getId());
+            assertThat(tracker.findById(item.getId()), is(nullValue()));
+
+        }
+    }
+
+    @Test
+    public void findByName() throws Exception {
+        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
+            Item item = new Item("test1");
+            tracker.add(item);
+            List<Item> result = tracker.findByName(item.getName());
+            assertThat(result.size(), is(1));
+
+        }
+    }
+
+    @Test
+    public void findById() throws Exception {
+        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
+            Item item = new Item("test1");
+            tracker.add(item);
+            Item result = tracker.findById(item.getId());
+            assertThat(result.getName(), is("test1"));
+
+        }
+    }
+
+    @Test
+    public void showListItems() throws Exception {
+        try (Store tracker = new SqlTracker(ConnectionRollback.create(SqlTracker.init()))) {
+            tracker.add(new Item("test1"));
+            tracker.add(new Item("test2"));
+            List<Item> result = tracker.findAll();
+            assertThat(result.size(), is(2));
+
+        }
+    }
+}
